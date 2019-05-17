3578f31a0a6a45fde4484f0832e51b22efa9775d
Paulo Silva <paulo.silva@cnec.br>
2019-05-17 11:57:49 -0300

    setando o ambiente (enviroment) do node, por algum motivo não esta pegando o ambiente de development ou homologation

--- src/config/consts.js
+++ src/config/consts.js
@@ -4,7 +4,7 @@ import _  from 'lodash';
 export const BASE_API = (
 
     process.env.NODE_ENV === 'production' ? 
-    'http://url-de-producao'
+    'http://hmlapidescontos.cnec.br'
     :
     process.env.NODE_ENV === 'homologation' ?
         'http://hmlapidescontos.cnec.br'
@@ -20,7 +20,7 @@ export const LOGIN_GOOGLE = (
             'client_id' : '560156040921-uo0006igf0kap726nmidjvb80bke3chq.apps.googleusercontent.com',
             'client_secret' : 'Sj6tSCVpP4E9jMBQ03df0dB4',
             'grant_type' : 'authorization_code',
-            'redirect_uri': 'http://localhost:3000/'
+            'redirect_uri': 'http://hmldescontos.cnec.br/'
        }
     : 
     process.env.NODE_ENV === 'homologation' ?

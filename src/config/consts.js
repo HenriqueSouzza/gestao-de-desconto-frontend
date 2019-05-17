2faad04f2010758b49a5690b23e29addac5ee061
Paulo Silva <paulo.silva@cnec.br>
2019-05-17 11:37:22 -0300

    ajustando as constantes para o ambiente de homologação

--- src/config/consts.js
+++ src/config/consts.js
@@ -6,8 +6,8 @@ export const BASE_API = (
     process.env.NODE_ENV === 'production' ? 
     'http://url-de-producao'
     :
-    process.env.NODE_ENV === 'homologacao' ?
-        'http://url-de-homologacao'
+    process.env.NODE_ENV === 'homologation' ?
+        'http://hmlapidescontos.cnec.br'
         :
         'http://localhost:8888/api'
 );
@@ -23,12 +23,12 @@ export const LOGIN_GOOGLE = (
             'redirect_uri': 'http://localhost:3000/'
        }
     : 
-    process.env.NODE_ENV === 'homologacao' ?
+    process.env.NODE_ENV === 'homologation' ?
         { 
             'client_id' : '560156040921-uo0006igf0kap726nmidjvb80bke3chq.apps.googleusercontent.com',
             'client_secret' : 'Sj6tSCVpP4E9jMBQ03df0dB4',
             'grant_type' : 'authorization_code',
-            'redirect_uri': 'http://localhost:3000/'
+            'redirect_uri': 'http://hmldescontos.cnec.br/'
         }
     :
         { 

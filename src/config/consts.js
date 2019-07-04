e4d9e13eb2c9ed550f6eaad3483240ed6ca81b42
Henrique Pereira de Souza <henrique.souza@cnecnacional.com.br>
2019-07-04 14:41:38 -0300

    correcao no arquivo consts

--- src/config/consts.js
+++ src/config/consts.js
@@ -1,12 +1,16 @@
 import _  from 'lodash';
 
+// export const BASE_API =  'http://hmlapidescontos.cnec.br/api';
+
+console.log(process.env);
 
 export const BASE_API = (
 
+    
     process.env.NODE_ENV === 'production' ? 
         'http://spcomapi.cnec.br/api'
     :
-    process.env.NODE_ENV === 'homologation' ?
+    process.env.NODE_ENV === 'test' ?
         'http://hmlapidescontos.cnec.br/api'
     :
         'http://localhost:8888/api'
@@ -20,10 +24,10 @@ export const LOGIN_GOOGLE = (
             'client_id' : '809505609882-8h36mosq038smdt4qh7c3jgidf95gpq1.apps.googleusercontent.com',
             'client_secret' : '0r7yKJ0PyQuiIZ-jjDQH1z2-',
             'grant_type' : 'authorization_code',
-            'redirect_uri': 'spcom.cnec.br'
+            'redirect_uri': 'http://spcom.cnec.br/'
        }
     : 
-    process.env.NODE_ENV === 'homologation' ?
+    process.env.NODE_ENV === 'test' ?
         { 
             'client_id' : '809505609882-8h36mosq038smdt4qh7c3jgidf95gpq1.apps.googleusercontent.com',
             'client_secret' : '0r7yKJ0PyQuiIZ-jjDQH1z2-',
